2ff129f69cf90f1797bd95cde1d72ff84da3d69a
jobs <yao050421103@163.com>
2021-02-23 15:39:35 +0800

    add ast test

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+	"bytes"
 	"hermes/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -31,6 +33,14 @@ func (this *Program) TokenLiteral() string {
 	return ""
 }
 
+func (this *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range this.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // Identifier : implement Expression
 type Identifier struct {
 	Tok   *token.Token
@@ -40,6 +50,9 @@ type Identifier struct {
 func (this *Identifier) expressionNode() {}
 func (this *Identifier) TokenLiteral() string {
 	return this.Tok.Literal
+}
+func (this *Identifier) String() string {
+	return this.Value
 }
 
 // VarStatement : implement Statement
@@ -53,6 +66,18 @@ func (this *VarStatement) statementNode() {}
 func (this *VarStatement) TokenLiteral() string {
 	return this.Tok.Literal
 }
+func (this *VarStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(this.TokenLiteral())
+	out.WriteString(" ")
+	out.WriteString(this.Name.String())
+	out.WriteString(" = ")
+	if nil != this.Value {
+		out.WriteString(this.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 // ReturnStatement : implement Statement
 type ReturnStatement struct {
@@ -63,4 +88,31 @@ type ReturnStatement struct {
 func (this *ReturnStatement) statementNode() {}
 func (this *ReturnStatement) TokenLiteral() string {
 	return this.Tok.Literal
+}
+func (this *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(this.TokenLiteral())
+	out.WriteString(" ")
+
+	if nil != this.ReturnValue {
+		out.WriteString(this.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Tok  *token.Token
+	Expr Expression
+}
+
+func (this *ExpressionStatement) statementNode() {}
+func (this *ExpressionStatement) TokenLiteral() string {
+	return this.Tok.Literal
+}
+func (this *ExpressionStatement) String() string {
+	if this.Expr != nil {
+		return this.Expr.String()
+	}
+	return ""
 }
---
+++ ast/ast_test.go
@@ -0,0 +1,27 @@
+package ast
+
+import (
+	"hermes/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: StatementSlice{
+			&VarStatement{
+				Tok: &token.Token{Type: token.VAR, Literal: "var"},
+				Name: &Identifier{
+					Tok:   &token.Token{Type: token.IDENT, Literal: "testVar1"},
+					Value: "testVar1",
+				},
+				Value: &Identifier{
+					Tok:   &token.Token{Type: token.IDENT, Literal: "testVar2"},
+					Value: "testVar2",
+				},
+			},
+		},
+	}
+	if program.String() != "var testVar1 = testVar2;" {
+		t.Errorf("program.String() wrong, got: %v", program.String())
+	}
+}
